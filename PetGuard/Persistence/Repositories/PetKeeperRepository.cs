0006f28e88d4001c969ea5237c794aee5f8234f8
Victor Fajardo <victor.fajardo.rojas@gmail.com>
2020-10-25 23:28:54 -0500

    All clases created (NOT implemented)

---
+++ PetGuard/Persistence/Repositories/PetKeeperRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PetGuard.Persistence.Repositories
+{
+    public class PetKeeperRepository
+    {
+    }
+}
