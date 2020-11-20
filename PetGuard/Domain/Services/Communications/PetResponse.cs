875bb66f27f4bafbb2a23e15220867d7f998cb28
Victor Fajardo <victor.fajardo.rojas@gmail.com>
2020-11-20 03:38:25 -0500

    All Responses implemented

--- PetGuard/Domain/Services/Communications/PetResponse.cs
+++ PetGuard/Domain/Services/Communications/PetResponse.cs
@@ -1,11 +1,19 @@
-﻿using System;
+﻿using PetGuard.Domain.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace PetGuard.Domain.Services.Communications
 {
-    public class PetResponse
+    public class PetResponse : BaseResponse<Pet>
     {
+        public PetResponse(Pet resource) : base(resource)
+        {
+        }
+
+        public PetResponse(string message) : base(message)
+        {
+        }
     }
 }
